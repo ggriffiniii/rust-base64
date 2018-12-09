48bf1760c3b693437ee5dbab12bb7593aefad21d
Glenn Griffin <ggriffiniii@gmail.com>
2018-12-08 21:21:51 -0800

    let the compiler unroll the decode chunk loop

--- src/encode/bulk_encoding/mod.rs
+++ src/encode/bulk_encoding/mod.rs
@@ -13,13 +13,22 @@ pub trait BulkEncoding: ::private::Sealed {
     fn bulk_encode(self, input: &[u8], output: &mut [u8]) -> (usize, usize);
 }
 
-// TODO: remove the dead_code bypass once a custom alphabet encoding is provided.
-#[allow(dead_code)]
+#[derive(Debug, Default, Copy, Clone)]
 pub struct ScalarBulkEncoding<C>(C);
-impl<C> ScalarBulkEncoding<C> {
+impl<C> ScalarBulkEncoding<C> where C: ::encode::Encoding {
     const INPUT_CHUNK_BYTES_READ: usize = 26;
     const INPUT_CHUNK_BYTES_ENCODED: usize = 24;
     const OUTPUT_CHUNK_BYTES_WRITTEN: usize = 32;
+
+    #[inline]
+    fn encode_chunk(self, input: u64, output: &mut [u8]) {
+        const LOW_SIX_BITS: u64 = 0x3F;
+        for byte_idx in 0..8 {
+            let shift_amount = 64 - (byte_idx as u64+1)*6;
+            let shifted_input = input >> shift_amount;
+            output[byte_idx] = self.0.encode_u6((shifted_input & LOW_SIX_BITS) as u8);
+        }
+    }
 }
 
 impl<C> BulkEncoding for ScalarBulkEncoding<C>
@@ -32,7 +41,6 @@ where
     fn bulk_encode(self, input: &[u8], output: &mut [u8]) -> (usize, usize) {
         let mut input_index: usize = 0;
 
-        const LOW_SIX_BITS: u64 = 0x3F;
 
         let last_fast_index: isize = input.len() as isize - Self::INPUT_CHUNK_BYTES_READ as isize;
         let mut output_index = 0;
@@ -51,51 +59,10 @@ where
             // SHA-512 digest), so it would be nice if that fit in the unrolled loop at least once.
             // Plus, single-digit percentage performance differences might well be quite different
             // on different hardware.
-
-            let input_u64 = BigEndian::read_u64(&input_chunk[0..]);
-
-            output_chunk[0] = self.0.encode_u6(((input_u64 >> 58) & LOW_SIX_BITS) as u8);
-            output_chunk[1] = self.0.encode_u6(((input_u64 >> 52) & LOW_SIX_BITS) as u8);
-            output_chunk[2] = self.0.encode_u6(((input_u64 >> 46) & LOW_SIX_BITS) as u8);
-            output_chunk[3] = self.0.encode_u6(((input_u64 >> 40) & LOW_SIX_BITS) as u8);
-            output_chunk[4] = self.0.encode_u6(((input_u64 >> 34) & LOW_SIX_BITS) as u8);
-            output_chunk[5] = self.0.encode_u6(((input_u64 >> 28) & LOW_SIX_BITS) as u8);
-            output_chunk[6] = self.0.encode_u6(((input_u64 >> 22) & LOW_SIX_BITS) as u8);
-            output_chunk[7] = self.0.encode_u6(((input_u64 >> 16) & LOW_SIX_BITS) as u8);
-
-            let input_u64 = BigEndian::read_u64(&input_chunk[6..]);
-
-            output_chunk[8] = self.0.encode_u6(((input_u64 >> 58) & LOW_SIX_BITS) as u8);
-            output_chunk[9] = self.0.encode_u6(((input_u64 >> 52) & LOW_SIX_BITS) as u8);
-            output_chunk[10] = self.0.encode_u6(((input_u64 >> 46) & LOW_SIX_BITS) as u8);
-            output_chunk[11] = self.0.encode_u6(((input_u64 >> 40) & LOW_SIX_BITS) as u8);
-            output_chunk[12] = self.0.encode_u6(((input_u64 >> 34) & LOW_SIX_BITS) as u8);
-            output_chunk[13] = self.0.encode_u6(((input_u64 >> 28) & LOW_SIX_BITS) as u8);
-            output_chunk[14] = self.0.encode_u6(((input_u64 >> 22) & LOW_SIX_BITS) as u8);
-            output_chunk[15] = self.0.encode_u6(((input_u64 >> 16) & LOW_SIX_BITS) as u8);
-
-            let input_u64 = BigEndian::read_u64(&input_chunk[12..]);
-
-            output_chunk[16] = self.0.encode_u6(((input_u64 >> 58) & LOW_SIX_BITS) as u8);
-            output_chunk[17] = self.0.encode_u6(((input_u64 >> 52) & LOW_SIX_BITS) as u8);
-            output_chunk[18] = self.0.encode_u6(((input_u64 >> 46) & LOW_SIX_BITS) as u8);
-            output_chunk[19] = self.0.encode_u6(((input_u64 >> 40) & LOW_SIX_BITS) as u8);
-            output_chunk[20] = self.0.encode_u6(((input_u64 >> 34) & LOW_SIX_BITS) as u8);
-            output_chunk[21] = self.0.encode_u6(((input_u64 >> 28) & LOW_SIX_BITS) as u8);
-            output_chunk[22] = self.0.encode_u6(((input_u64 >> 22) & LOW_SIX_BITS) as u8);
-            output_chunk[23] = self.0.encode_u6(((input_u64 >> 16) & LOW_SIX_BITS) as u8);
-
-            let input_u64 = BigEndian::read_u64(&input_chunk[18..]);
-
-            output_chunk[24] = self.0.encode_u6(((input_u64 >> 58) & LOW_SIX_BITS) as u8);
-            output_chunk[25] = self.0.encode_u6(((input_u64 >> 52) & LOW_SIX_BITS) as u8);
-            output_chunk[26] = self.0.encode_u6(((input_u64 >> 46) & LOW_SIX_BITS) as u8);
-            output_chunk[27] = self.0.encode_u6(((input_u64 >> 40) & LOW_SIX_BITS) as u8);
-            output_chunk[28] = self.0.encode_u6(((input_u64 >> 34) & LOW_SIX_BITS) as u8);
-            output_chunk[29] = self.0.encode_u6(((input_u64 >> 28) & LOW_SIX_BITS) as u8);
-            output_chunk[30] = self.0.encode_u6(((input_u64 >> 22) & LOW_SIX_BITS) as u8);
-            output_chunk[31] = self.0.encode_u6(((input_u64 >> 16) & LOW_SIX_BITS) as u8);
-
+            for chunk in 0..4 {
+                let input = BigEndian::read_u64(&input_chunk[chunk*6..]);
+                self.encode_chunk(input, &mut output_chunk[chunk*8..chunk*8+8]);
+            }
             input_index += Self::INPUT_CHUNK_BYTES_ENCODED;
             output_index += Self::OUTPUT_CHUNK_BYTES_WRITTEN;
         }
